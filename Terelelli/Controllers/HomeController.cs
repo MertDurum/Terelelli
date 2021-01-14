83386170329448286cb9aa4df6e05d9f63073b6e
Furkan Okuk <furkanokuk7@hotmail.com>
2021-01-14 20:56:59 +0300

    temsili anasayfa ve proje sayfası

--- Terelelli/Controllers/HomeController.cs
+++ Terelelli/Controllers/HomeController.cs
@@ -13,6 +13,11 @@ namespace Terelelli.Controllers
             return View();
         }
 
+        public ActionResult Proje()
+        {
+            return View();
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
